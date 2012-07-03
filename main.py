554a8402d41f4dd7591383cdc5acdd5d533129d2
Phillip B Oldham <phillip.oldham@exstatik.com>
2012-07-03 16:55:03 +0100

    small reorganisation;

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tifffile import TIFFfile
 
 def main():
-    for page in TIFFfile('ullswater.tiff'):
+    for page in TIFFfile('test.tiff'):
         for tag in page.tags.values():
             if not tag.name.isdigit():
                 print tag.name, tag.code, tag.dtype, tag.count, tag.value
